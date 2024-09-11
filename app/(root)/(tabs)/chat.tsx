f4aa79dfd271ef5d922e01f235759d5f5f773dfe
gokhan <mertgokhandonmez@gmail.com>
2024-09-11 17:08:52 +0300

    init

---
+++ app/(root)/(tabs)/chat.tsx
@@ -0,0 +1,11 @@
+import { View, Text, SafeAreaView } from "react-native";
+
+const Chat = () => {
+  return (
+    <SafeAreaView>
+      <Text>Chat</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Chat;
